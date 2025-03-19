9b443e5b7b679571a7d7af4c29a2251dbf6ec063
mathuzin <moplgalvao@furb.br>
2025-03-19 15:59:33 -0300

    Model de Aluno feito

--- projeto-esqueleto/src/main/java/br/furb/inf/furbot/models/usuarios/Aluno.java
+++ projeto-esqueleto/src/main/java/br/furb/inf/furbot/models/usuarios/Aluno.java
@@ -1,5 +1,35 @@
 package br.furb.inf.furbot.models.usuarios;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "aluno")
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nm_aluno", length = 100, nullable = false)
+    private String nome;
     
+    @Column(name = "email", length = 100, nullable = false)
+    private String email;
+
+    private Orientador orientador;
 }
