c49f07009519d8b7830501f1ec900c382e17784b
mathuzin <moplgalvao@furb.br>
2025-03-19 16:44:14 -0300

    Models prontos

--- gtfurb/src/main/java/com/example/gtfurb/models/Coordenador.java
+++ gtfurb/src/main/java/com/example/gtfurb/models/Coordenador.java
@@ -1,5 +1,32 @@
-package main.java.com.example.gtfurb;
+package com.example.gtfurb.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "coordenador")
 public class Coordenador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "nm_coordenador", length = 100, nullable = false)
+    private String nome;
+    
+    @Column(name = "email_coordenador", length = 100, nullable = false)
+    private String email;
     
 }
