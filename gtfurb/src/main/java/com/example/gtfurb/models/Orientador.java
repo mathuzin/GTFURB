619e02cddd8e5a858c7598d047857a27ecbe3b1e
mathuzin <moplgalvao@furb.br>
2025-03-20 15:47:11 -0300

    Arrumando classes de segurança

--- gtfurb/src/main/java/com/example/gtfurb/models/Orientador.java
+++ gtfurb/src/main/java/com/example/gtfurb/models/Orientador.java
@@ -1,14 +1,14 @@
 package com.example.gtfurb.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
