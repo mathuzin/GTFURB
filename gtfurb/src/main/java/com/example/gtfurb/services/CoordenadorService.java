b3335eca8d5b2a9ecc413efde97be7cc43e0bc8d
joaoeduardo <jectrevsiol@furb.br>
2025-03-24 16:32:18 -0300

    Método criarRelatorio - Coordenador

--- gtfurb/src/main/java/com/example/gtfurb/services/CoordenadorService.java
+++ gtfurb/src/main/java/com/example/gtfurb/services/CoordenadorService.java
@@ -1,28 +1,38 @@
 package com.example.gtfurb.services;
 
+import java.time.LocalDate;
+
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.gtfurb.models.Relatorio;
 import com.example.gtfurb.models.Usuarios.Aluno;
 import com.example.gtfurb.models.Usuarios.Orientador;
 import com.example.gtfurb.repository.AlunoRepository;
 import com.example.gtfurb.repository.CoordenadorRepository;
 import com.example.gtfurb.repository.OrientadorRepository;
+import com.example.gtfurb.repository.RelatorioRepository;
 
 @Service
 public class CoordenadorService {
 
     @Autowired
     private CoordenadorRepository coordenadRepository;
+
     @Autowired
     private BCryptPasswordEncoder bCript;
+
     @Autowired
     private OrientadorRepository orientadorRepository;
+
     @Autowired
     private AlunoRepository alunoRepository;
+
+    @Autowired
+    private RelatorioRepository relatorioRepository;
 
     CoordenadorService() {
 
@@ -44,5 +54,19 @@ public class CoordenadorService {
         aluno.setOrientador(orientador);
 
         return alunoRepository.save(aluno);
+    }
+
+    public Relatorio criarRelatorio(String titulo, LocalDate dataInicio, LocalDate dataTermino) {
+
+        if (titulo.isEmpty() || titulo == null) {
+            throw new IllegalArgumentException("Título é obrigatório.");
+        }
+
+        Relatorio relatorio = new Relatorio();
+        relatorio.setTitulo(titulo);
+        relatorio.setDataInicio(dataInicio);
+        relatorio.setDataFim(dataTermino);
+
+        return relatorioRepository.save(relatorio);
     }
 }
