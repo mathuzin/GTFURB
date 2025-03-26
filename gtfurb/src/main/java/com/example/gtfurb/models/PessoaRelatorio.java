2d858082db1526e3e645696b257abc22b2b718fd
mathuzin <moplgalvao@furb.br>
2025-03-26 16:34:35 -0300

    Arrumando model PessoaRelatorio

--- gtfurb/src/main/java/com/example/gtfurb/models/PessoaRelatorio.java
+++ gtfurb/src/main/java/com/example/gtfurb/models/PessoaRelatorio.java
@@ -5,10 +5,10 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -22,18 +22,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "pessoa_relatorio")
-// @IdClass(PessoaRelatorioId.class) 
 public class PessoaRelatorio {
 
     @Id
-    @ManyToOne
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JoinColumn(name = "id_pessoa", nullable = false, foreignKey = @ForeignKey(name = "pessoa_id_pessoa"))
-    private Pessoa pessoa;
+    private Integer idPessoa;
 
     @Id
-    @ManyToOne
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JoinColumn(name = "id_relatorio", nullable = false, foreignKey = @ForeignKey(name = "relatorio_id_relatorio"))
-    private Relatorio relatorio;
+    private Long idRelatorio;
 
     @NotBlank(message = "O campo de texto não pode estar vazio")
     @Size(max = 500, message = "O texto deve ter no máximo 5000 caracteres")
